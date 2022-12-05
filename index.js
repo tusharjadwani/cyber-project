10c8ff5015e652c01eb7b7ba8e980c72266fb2b7
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2022-12-05 18:23:12 +0530

    up3

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(express.static(path.join(__dirname,'./client/build')))
-app.get("*",()=>(req,res)=>{
+
+app.get("/",()=>(req,res)=>{
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+app.get("/admin",()=>(req,res)=>{
   res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
